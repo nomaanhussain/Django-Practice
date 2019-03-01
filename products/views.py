648c996fe2c4e1eecd624093e721e9670f409050
nomaanhussain <nomaanghanchi_11@yahoo.com>
2019-03-01 20:39:02 +0500

    model created

--- products/views.py
+++ products/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from .models import Product
 # Create your views here.
+from .forms import ProductForm
+
+def product_create_view(requset):
+    form = ProductForm(requset.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+
+    context = {
+        'form': form
+    }
+    return render(requset, 'products/create.html', context)
 
 def product_detail_view(requset):
     obj = Product.objects.get(id=1)
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view
-from products.views import product_detail_view
+from products.views import product_detail_view, product_create_view
 
 urlpatterns = [
+    path('create/', product_create_view),
     path('product/', product_detail_view),
     path('', home_view, name = 'home'),
     path('admin/', admin.site.urls),
